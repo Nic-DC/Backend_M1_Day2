0c2ef9d2b6e60c4de86b4dc559879110ce02b611
Nic <nic.dan.costea@gmail.com>
2023-01-10 17:35:10 +0100

    env file update - fe urls

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import authorsRouter from "./api/authors/index.js";
 import postsRouter from "./api/blogPosts/index.js";
 import filesRouter from "./api/files/index.js";
 import listEndpoints from "express-list-endpoints";
+import createHttpError from "http-errors";
+
+const { NotFound } = createHttpError;
 
 import { join } from "path";
 
@@ -14,10 +17,23 @@ const server = express();
 
 const port = process.env.PORT;
 
+const whiteList = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+
+const corsOptions = {
+  origin: (origin, corsNext) => {
+    console.log("CURRENT ORIGIN", origin);
+    if (!origin || whiteList.indexOf(origin) !== -1) {
+      corsNext(null, true);
+    } else {
+      corsNext(NotFound(`Origin ${origin} is not in the whitelist`));
+    }
+  },
+};
+
 const publicFolderPath = join(process.cwd(), "./public");
 
 server.use(express.static(publicFolderPath));
-server.use(cors()); // Just to let FE communicate with BE successfully
+server.use(cors(corsOptions)); //
 server.use(express.json());
 
 /*-----------ENDPOINTS-------------*/
